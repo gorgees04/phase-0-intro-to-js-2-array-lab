a10cd29cbb97d5355c76dcb25ace6696be6fea1d
Gorgees Odisho <gorgees041@gmail.com>
2023-09-28 03:34:06 +0300

    DONE WITH ALL ARR

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name)
+}
+
+function appendCat(name){
+    const newArr = [...cats, name]
+    return newArr
+}
+
+function prependCat(name){
+    const newArr = [name, ...cats]
+    return newArr
+}
+
+function removeLastCat() {
+    return cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
